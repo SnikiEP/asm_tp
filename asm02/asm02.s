12dbafa4b7e42049b4b81da23206bd376d72946d
SnikiEP <enzopizzuti17@hotmail.fr>
2025-10-03 15:27:00 +0200

    fix asm02

--- asm02/asm02.s
+++ asm02/asm02.s
@@ -6,41 +6,60 @@ section .data
     output_len equ $ - output_msg
 
 section .bss
-    input_buf resb 8
+    input_buffer resb 64
 
 section .text
     global _start
 
 _start:
-    mov rax, 0            
-    mov rdi, 0            
-    mov rsi, input_buf    
-    mov rdx, 8            
+    mov     rax, 0
+    mov     rdi, 0
+    mov     rsi, input_buffer
+    mov     rdx, 64
     syscall
-    mov rbx, rax          
+    mov     rbx, rax
 
-    mov rcx, ref_input_len
-    cmp rbx, rcx
-    jne exit_fail
+    mov     rcx, rbx
+    mov     rdi, input_buffer
+find_newline:
+    cmp     rcx, 0
+    je      no_newline
+    mov     al, byte [rdi]
+    cmp     al, 0x0A
+    je      truncate_line
+    inc     rdi
+    dec     rcx
+    jmp     find_newline
 
-    mov rsi, input_buf
-    mov rdi, ref_input
-    mov rcx, ref_input_len
-    repe cmpsb
-    jne exit_fail
+truncate_line:
+    mov     byte [rdi+1], 0
+    mov     rbx, rdi
+    sub     rbx, input_buffer
+    add     rbx, 1
+
+no_newline:
+    mov     rcx, ref_input_len
+    cmp     rbx, rcx
+    jne     exit_fail
+
+    mov     rsi, input_buffer
+    mov     rdi, ref_input
+    mov     rcx, ref_input_len
+    repe    cmpsb
+    jne     exit_fail
 
 exit_success:
-    mov rax, 1            
-    mov rdi, 1            
-    mov rsi, output_msg
-    mov rdx, output_len
+    mov     rax, 1
+    mov     rdi, 1
+    mov     rsi, output_msg
+    mov     rdx, output_len
     syscall
 
-    xor rdi, rdi
-    mov rax, 60           
+    mov     rax, 60
+    xor     rdi, rdi
     syscall
 
 exit_fail:
-    mov rdi, 1
-    mov rax, 60           
+    mov     rax, 60
+    mov     rdi, 1
     syscall
